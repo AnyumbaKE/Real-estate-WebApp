9d5160fe36bb80e1a364d0c5fe91c6cf51093122
AnyumbaKE <esanyumba@gmail.com>
2024-06-30 18:47:49 +0300

    boma.py

--- boma/utils.py
+++ boma/utils.py
@@ -14,4 +14,8 @@ def unique_slug_generator(instance, new_slug=None):
         
         Klass = instance.__class__
         qs_exists = Klass.objects.filter(slug=slug).exists()
-        
+        if qs_exists:
+            new_slug = "{slug}-{randstr}".format(
+                slug=slug,
+                randstr=random_string_generator(size=4)
+            )
