7b196cc6cea5f64df408a2de4c45e50172950de9
AnyumbaKE <esanyumba@gmail.com>
2024-06-30 17:40:22 +0300

    settings.py

--- boma/settings.py
+++ boma/settings.py
@@ -137,7 +137,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'boma/static'))]
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Media Folder Settings
