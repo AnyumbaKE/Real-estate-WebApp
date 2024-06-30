6c7a04c2681175b40b45e6b7c0143ce207f49fec
AnyumbaKE <esanyumba@gmail.com>
2024-06-30 20:47:39 +0300

    blog

--- blog/schema.py
+++ blog/schema.py
@@ -18,6 +18,9 @@ class UpdatePost(graphene.Mutation):
         id = graphene.Int(required=True)
         input = PostInput(required=True)
     
+    ok = graphene.Boolean()
+    post = graphene.Field(PostType)
+    
 class Mutation(graphene.ObjectType):
     create_category = CreateCategories.Field()
     update_category = UpdateCategories.Field()
