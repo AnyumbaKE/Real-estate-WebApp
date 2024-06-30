d4758431d32cd61abee01592a0631c6d968d64bc
AnyumbaKE <esanyumba@gmail.com>
2024-06-30 19:12:22 +0300

    blog

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,8 @@ from datetime import datetime, date
 from django.db.models.base import Model
 from django.db.models.signals import pre_save
 from boma.utils import unique_slug_generator
+
+class
+class
+class
+class Post(models.Model):
