f54e86e31f2874f9a667a35b8d5bda571b4255ba
AnyumbaKE <esanyumba@gmail.com>
2024-06-30 19:04:39 +0300

    blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post, categories, PostComment
+from .models import Post, Categories, PostComment
 
 admin.site.register(Post)
 admin.site.register(PostComment)
+admin.site.register(Categories)
