173ba14288ff2c017bc379346097a86cb9c39b9c
AnyumbaKE <esanyumba@gmail.com>
2024-06-30 18:37:33 +0300

    boma.py

--- boma/utils.py
+++ boma/utils.py
@@ -3,7 +3,7 @@ import random
 from django.utils.text import slugify
 
 def random_string_generator(size=10, chars=string.ascii_lowercase + string.digits):
-    
+    return ''.join(random.choice(chars) for _ in range(size))
 
 
 def
