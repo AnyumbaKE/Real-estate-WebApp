11311ceb103795f6fef4d2b1c312ae7844f82aae
AnyumbaKE <esanyumba@gmail.com>
2024-06-30 19:10:44 +0300

    blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime, date
+from django.db.models.base import Model
+from django.db.models.signals import pre_save
 from boma.utils import unique_slug_generator
